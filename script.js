e9ae300894f54e3e9e15b98236e1e7133b862321
Edison <130329661+Wyncroft18@users.noreply.github.com>
2023-08-31 17:58:39 +0800

    Create a single-round function of rock-paper-scissor

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-function getComputerChoice () {
-    const choices = ["Rock", "Paper", "Scissor"];
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissor"];
     const random = Math.floor(Math.random() * choices.length);
-    console.log(choices[random]);
+    const choice = choices[random];
+    return choice;
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection) {
+        console.log("It's a draw!");
+    }
+    else {
+        if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
+            console.log("You lose! Paper beats rock.");
+        } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissor") {
+            console.log("You win! Rock beats scissor.");
+        } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+            console.log("You win! Paper beats rock.");
+        } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissor") {
+            console.log("You lose! Scissor beats paper.");
+        } else if (playerSelection.toLowerCase() === "scissor" && computerSelection === "rock") {
+            console.log("You lose! Rock beats scissor.");
+        } else {
+            console.log("You win! Scissor beats paper.")
+        }
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice()
+console.log(playRound(playerSelection, computerSelection));
